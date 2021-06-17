eff733c8dea65bd2aad167f98798138388486654
bo12s <botolfs@gmail.com>
2021-06-17 14:57:06 +0200

    install script

--- install.ps1
+++ install.ps1
@@ -1,3 +1,42 @@
-$valheimPath = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 892970" -Name InstallLocation
-$bepinex = $valheimPath+"\BepInEx\"
+try { 
+    $valheimPath = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 892970" -Name InstallLocation
+    $bepinex = $valheimPath+"\BepInEx" 
+    $configPath = $bepinex+"\config\" 
+    $pluginsPath = $bepinex+"\plugins\" 
+    $patchersPath = $bepinex+"\patchers\" 
+}
+catch { 
+    "Couldn't find Valheim folder through registry, install manually."
+    return 
+}
+
+Write-Host "BepInEx folder found:"
 Write-Host $bepinex
+Write-Host 
+
+
+RemoveFolder -targetFolder $configPath
+RemoveFolder -targetFolder $pluginsPath
+CopyToBepInEx -source .\config
+CopyToBepInEx -source .\plugins
+
+function RemoveFolder{
+    param($targetFolder)
+
+    Write-Host "Removing:"$targetFolder
+
+    if (Test-Path $targetFolder) {        
+        Remove-Item $targetFolder -Recurse
+        Write-Host "Done!"
+    }
+    else
+    {
+        Write-Host "Not found."
+    }
+}
+
+function CopyToBepInEx{
+    param($source)
+
+    Copy-Item $source -Destination $bepinex -Recurse
+}
